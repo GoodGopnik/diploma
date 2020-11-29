2e3ca35549d317330240a785501b2ea1ea094f2c
GoodGopmik <swig11111@gmail.com>
2020-11-29 22:33:15 +0200

    сделал кнопку и динамику для блока новое поступление

--- app/js/main.js
+++ app/js/main.js
@@ -44,6 +44,15 @@ async function dataAdd () {
     const text4 = document.getElementById ('text4')
     const text5 = document.getElementById ('text5')
     const text6 = document.getElementById ('text6')
+    const text7 = document.getElementById ('text7')
+    const text8 = document.getElementById ('text8')
+    const text9 = document.getElementById ('text9')
+    const text10 = document.getElementById ('text10')
+    const text11 = document.getElementById ('text11')
+    const text12 = document.getElementById ('text12')
+    const text13 = document.getElementById ('text13')
+    const text14 = document.getElementById ('text14')
+    const button = document.getElementById ('button')
 
     let key //создание переменной для цикла
     for(key in resault) {  //создали цикл и вставили туда объект по чьим свойствам мы пройдемся. Сам цикл for..in
@@ -85,5 +94,60 @@ async function dataAdd () {
                 text5.insertAdjacentElement('afterend', newElem)
                 text6.insertAdjacentElement('afterend', newElem)
             }  
+    button.onclick = function () {
+        for(key in resault) {  //создали цикл и вставили туда объект по чьим свойствам мы пройдемся. Сам цикл for..in
+
+            let newElem = document.createElement ('div')
+                
+            newElem.innerHTML = `
+                <a data-fancybox="gallery" href="/img/season/620501.png">
+                <img src="./img/season/620501.png" alt="" id="img1"></a>
+                <p>${resault[key].title}</p>
+                <p>${resault[key].id}</p>
+                `
+                text7.insertAdjacentElement('afterend', newElem)
+                text8.insertAdjacentElement('afterend', newElem)
+            }  
+            for(key in resault) {  //создали цикл и вставили туда объект по чьим свойствам мы пройдемся. Сам цикл for..in
+
+                let newElem = document.createElement ('div')
+                    
+                newElem.innerHTML = `
+                    <a data-fancybox="gallery" href="/img/season/620501.png">
+                    <img src="./img/season/620501.png" alt="" id="img1"></a>
+                    <p>${resault[key].title}</p>
+                    <p>${resault[key].id}</p>
+                    `
+                    text9.insertAdjacentElement('afterend', newElem)
+                    text10.insertAdjacentElement('afterend', newElem)
+                } 
+                for(key in resault) {  //создали цикл и вставили туда объект по чьим свойствам мы пройдемся. Сам цикл for..in
+
+                    let newElem = document.createElement ('div')
+                        
+                    newElem.innerHTML = `
+                        <a data-fancybox="gallery" href="/img/season/620501.png">
+                        <img src="./img/season/620501.png" alt="" id="img1"></a>
+                        <p>${resault[key].title}</p>
+                        <p>${resault[key].id}</p>
+                        `
+                        text11.insertAdjacentElement('afterend', newElem)
+                        text12.insertAdjacentElement('afterend', newElem)
+                    } 
+                    for(key in resault) {  //создали цикл и вставили туда объект по чьим свойствам мы пройдемся. Сам цикл for..in
+
+                        let newElem = document.createElement ('div')
+                            
+                        newElem.innerHTML = `
+                            <a data-fancybox="gallery" href="/img/season/620501.png">
+                            <img src="./img/season/620501.png" alt="" id="img1"></a>
+                            <p>${resault[key].title}</p>
+                            <p>${resault[key].id}</p>
+                            `
+                            text13.insertAdjacentElement('afterend', newElem)
+                            text14.insertAdjacentElement('afterend', newElem)
+                        } 
+    }
+
 }      
 dataAdd() 
